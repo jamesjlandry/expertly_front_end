3c6903130e1f1373a20711f0854a8b374dd579c3
rhinehardtc <rhinehardtc@gmail.com>
2020-08-20 17:27:29 -0500

    Included additional fields-container styling

--- src/containers/FieldsContainer.js
+++ src/containers/FieldsContainer.js
@@ -8,8 +8,12 @@ export default class FieldsContainer extends React.Component {
 
         return (
             <div className="expert_fields">
-                Filter Questions
-                <div onClick={() => this.props.unfilterField()} >All</div>
+                <div className="expert_fields_header">
+                    <h4>Filter Questions</h4>
+                </div>
+                <div className="field_li" onClick={() => this.props.unfilterField()} >
+                    <div className="inner_field_li">All</div>
+                </div>
             
                 {this.props.fields.map(field => <Field key={field.id} field={field} filterField={this.props.filterField} />)}
             </div>
